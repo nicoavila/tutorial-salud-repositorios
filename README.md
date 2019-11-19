d63ea386e21566b39962c715511e8796325f6a6d
Nicolas Avila <nicolasavila.a@gmail.com>
2019-11-19 11:29:20 -0300

    feat(project): commit inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fomentando una mejor salud para tus repositorios
+Repositorio con el código utilizado para el artículo **Fomentando una mejor salud para tus repositorios**
+
+![Portada](/images/portada.png)
+
+## Instalación
+1. Clonar el repositorio
+2. Ejecutar `npm install``
+
+(TO-DO)
---
+++ app.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.hex('#F60000')('Mentholatum doesn\'t relieve CS gas effects'));
+console.log(chalk.hex('#FF8C00')('Mentholatum doesn\'t relieve CS gas effects'));
+console.log(chalk.hex('#FFEE00')('Mentholatum doesn\'t relieve CS gas effects'));
+console.log(chalk.hex('#4DE94C')('Mentholatum doesn\'t relieve CS gas effects'));
+console.log(chalk.hex('#3783FF')('Mentholatum doesn\'t relieve CS gas effects'));
+console.log(chalk.hex('#4815AA')('Mentholatum doesn\'t relieve CS gas effects'));
